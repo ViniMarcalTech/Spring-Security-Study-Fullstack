e3136edc0c979b91a3cb1fbcd6e3b40171cf33d3
ViniMarcal.tech <vinicius.dm1515@gmail.com>
2024-04-25 11:43:16 -0300

    JWT Authentication and validation email layers

---
+++ spring-security-study/src/main/java/com/securitystudy/auth/RegistrationRequest.java
@@ -0,0 +1,30 @@
+package com.securitystudy.auth;
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Builder
+public class RegistrationRequest {
+
+    @NotEmpty(message = "Firstname is mandatory")
+    @NotBlank(message = "Firstname is mandatory")
+    private String firstname;
+    @NotEmpty(message = "Lastname is mandatory")
+    @NotBlank(message = "Lastname is mandatory")
+    private String lastname;
+    @Email(message = "Email is not formatted")
+    @NotEmpty(message = "Email is mandatory")
+    @NotBlank(message = "Email is mandatory")
+    private String email;
+    @Size(min = 8, message = "Password should be 8 characters long minimum")
+    private String password;
+
+
+}
