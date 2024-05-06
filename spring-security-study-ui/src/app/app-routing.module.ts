237eb5c07099b45c96237ee7a73e48d33fd587af
ViniMarcal.tech <vinicius.dm1515@gmail.com>
2024-05-06 15:27:55 -0300

    Activate account Page + Hello World

--- spring-security-study-ui/src/app/app-routing.module.ts
+++ spring-security-study-ui/src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { ActivateAccountComponent } from './pages/activate-account/activate-account.component';
+import { HelloWorldComponent } from './pages/hello-world/hello-world.component';
 
 const newLocal = 'login';
 const routes: Routes = [
@@ -12,6 +14,14 @@ const routes: Routes = [
   {
     path: 'register',
     component: RegisterComponent,
+  },
+  {
+    path: 'activate-account',
+    component: ActivateAccountComponent,
+  },
+  {
+    path: 'hello-world',
+    component: HelloWorldComponent,
   },
 ];
 
--- spring-security-study-ui/src/app/app.module.ts
+++ spring-security-study-ui/src/app/app.module.ts
@@ -7,10 +7,25 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { ActivateAccountComponent } from './pages/activate-account/activate-account.component';
+import { CodeInputModule } from 'angular-code-input';
+import { HelloWorldComponent } from './pages/hello-world/hello-world.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, RegisterComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ActivateAccountComponent,
+    HelloWorldComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    CodeInputModule,
+  ],
   providers: [HttpClient],
   bootstrap: [AppComponent],
 })
